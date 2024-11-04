ca0ceb00dc2580718916c1985d3dd61ec6b55683
Alex-Prospa <alexander_moses@hotmail.com>
2024-11-04 22:26:15 +1100

    Initialised Swift App

---
+++ Financly/Financly/FinanclyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanclyApp.swift
+//  Financly
+//
+//  Created by Alex Moses on 4/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct FinanclyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Financly/FinanclyTests/FinanclyTests.swift
@@ -0,0 +1,17 @@
+//
+//  FinanclyTests.swift
+//  FinanclyTests
+//
+//  Created by Alex Moses on 4/11/2024.
+//
+
+import Testing
+@testable import Financly
+
+struct FinanclyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
